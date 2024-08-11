b276f6e03b58f819d6e3d3103d96fa82ad649c88
Andrew Drake <adrake@adrake.org>
2024-08-11 13:42:15 -0700

    Add UBSan support

--- build_args.sh
+++ build_args.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -eu
 
-if [[ "$#" -ne 2 ]]; then
-    echo "Usage: $0 <asan> <debug>" >&2
+if [[ "$#" -ne 3 ]]; then
+    echo "Usage: $0 <asan> <debug> <ubsan>" >&2
     exit 1
 fi
 asan="$1"
 debug="$2"
+ubsan="$3"
 
 function enabled() {
     [[ "$1" == "true" ]]
@@ -15,25 +16,30 @@ flags=()
 suffix=""
 
 # Handle sanitizers
-if enabled "$asan"; then
-    suffix="${suffix}-asan"
-    flags+=(--enable-asan)
+enabled "$asan" && flags+=(--enable-asan)
+enabled "$ubsan" && flags+=(--enable-ubsan)
+if enabled "$asan" && enabled "$ubsan"; then
+    suffix+="-uasan"
+elif enabled "$asan"; then
+    suffix+="-asan"
+elif enabled "$ubsan"; then
+    suffix+="-ubsan"
 fi
 
 # Handle debug
 if enabled "$debug"; then
-    suffix="${suffix}-debug"
     flags+=(--debug --v8-with-dchecks)
+    suffix+="-debug"
 fi
 
 # Handle no flags
 if [[ -z "$suffix" ]]; then
-    suffix="-vanilla"
+    suffix+="-vanilla"
 fi
 
-echo "Configuration flags: ${flags[@]}"
+echo "Configuration flags: ${flags[*]}"
 echo "Image tag suffix: ${suffix}"
 if [[ -n "${GITHUB_OUTPUT:-}" ]]; then
-    echo "configFlags=${flags[@]}" >> "$GITHUB_OUTPUT"
+    echo "configFlags=${flags[*]}" >> "$GITHUB_OUTPUT"
     echo "tagSuffix=${suffix}" >> "$GITHUB_OUTPUT"
 fi
